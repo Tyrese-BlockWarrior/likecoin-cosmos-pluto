1ae56845d737febdf5bf25d9c926a964ecb88872
TyreseLian <tyreselian3915@gmail.com>
2022-08-17 19:02:18 +0800

    Add CSP meta tag in HTML (#2)
    * 🔒️ Add CSP meta tag in HTML
    
    * 🔒️ Allow `data:` protocol in `connect-src` for CSP

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createHtmlPlugin } from 'vite-plugin-html'
 import { resolve } from 'path';
+import { RPC_ENDPOINT } from './src/config'
+
+const url = new URL(RPC_ENDPOINT);
+const rpcDomain = url.host;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    createHtmlPlugin({
+      minify: true,
+      inject: {
+        data: { rpcDomain },
+      },
+    })
+  ],
   server: {
     host: '0.0.0.0',
   },
